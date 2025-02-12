2c699a40727c1be14027865994bbd81363334afe
pshptr <petia3711@gmail.com>
2025-02-12 12:36:12 +0300

    Initial Commit

---
+++ Calculator-iOS/Calculator_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_iOSApp.swift
+//  Calculator-iOS
+//
+//  Created by Pe Tia on 12.02.25.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
